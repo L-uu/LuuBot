6de9e0853c1cc9a9cc0b99079eb9951a4446372b
IronClawTrem <louie.nutman@gmail.com>
2021-05-26 15:28:50 +0100

    add claim command

---
+++ commands/claim.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: "claim",
+    execute(message) {
+        const db = require("quick.db");
+        const bankCheck = db.get(`guild_${message.guild.id}_bank_${message.author.id}`) || 0;
+        if(bankCheck == 0) {
+            message.channel.send("You don't have a bank, please create one.")
+                .then(msg => msg.delete({ timeout: 5000 }));
+            return message.react("❌");
+        };
+        const now = new Date();
+        const lastClaimed = db.add(`guild_${message.guild.id}_lastclaimed_${message.author.id}`, now.getMinutes());
+        if(now.getMinutes() > lastClaimed + 10) {
+            db.add(`guild_${message.guild.id}_bal_${message.author.id}`, 100);
+            message.channel.send("Successfully claimed $100, come back in 10 minutes.")
+                .then(msg => msg.delete({ timeout: 5000 }));
+            return message.react("✅");
+        } else {
+            message.channel.send("This command can only be used once every 10 minutes.")
+                .then(msg => msg.delete({ timeout: 5000 }));
+            return message.react("❌");
+        }
+    }
+};
