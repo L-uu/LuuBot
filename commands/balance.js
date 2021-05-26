0e40298ad1fcfd9ea80f1add79c777f1c237902c
IronClawTrem <louie.nutman@gmail.com>
2021-05-26 18:05:15 +0100

    balance command: fix once and for all smh

--- commands/balance.js
+++ commands/balance.js
@@ -3,15 +3,15 @@ module.exports = {
     aliases: ["bal"],
     execute(message) {
         const db = require("quick.db");
-        var bankCheck = db.get(`guild_${message.guild.id}_bank_${message.author.id}`) || 0;
-        if(bankCheck == 0) {
+        var bankCheck = db.get(`guild_${message.guild.id}_bank_${message.author.id}`);
+        if(!bankCheck) {
             message.channel.send("You don't have a bank, please create one.")
                 .then(msg => msg.delete({ timeout: 5000 }));
             return message.react("❌");
         };
         const user = message.mentions.users.first() || message.author;
-        var balCheck = db.get(`guild_${message.guild.id}_bank_${user.id}`);
-        if(!balCheck) {
+        var balCheck = db.get(`guild_${message.guild.id}_bal_${user.id}`);
+        if(balCheck == undefined) {
             message.channel.send("This user has no bank account.")
                 .then(msg => msg.delete({ timeout: 5000 }));
             return message.react("❌");
