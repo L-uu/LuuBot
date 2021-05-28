d5af2372c0cdab775582d896072e2c3f35998c7e
IronClawTrem <louie.nutman@gmail.com>
2021-05-28 17:56:30 +0100

    skip command: tweak last commit

--- commands/skip.js
+++ commands/skip.js
@@ -16,6 +16,11 @@ module.exports = {
         };
         try {
             if (!isNaN(args[0]) && args[0] >= 2) {
+                if (args[0] > serverQueue.songs.length) {
+                    message.channel.send("Invalid selection.")
+                        .then(msg => msg.delete({ timeout: 5000 }));
+                    return message.react("❌");
+                };
                 serverQueue.songs.splice(args[0] - 1, 1);
                 return message.react("✅");
             };
